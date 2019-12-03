2960ffbd7d1e2fea6b6efe7327c57e39d378377a
julia <julialitvachuk@gmail.com>
2019-12-02 19:37:04 -0500

    Turning in assignments

---
+++ assignments/Assignment 1.py
@@ -0,0 +1,15 @@
+favcandy1 = "Pelon Pelo Rico"
+favcandy2 = "Lucas"
+favcandy3 = "Chamoy raspado"
+favcandy4 = "Vero Mango"
+
+timeofday = input("Enter the time of day (M,A,E)")
+
+if(timeofday) == "M":
+    print("I'll eat " + favcandy1 + " in the morning.")
+elif(timeofday) == "A":
+    print("I'll eat " + favcandy2 + " in the afternoon.")
+elif(timeofday) == "E":
+    print("I'll eat a " + favcandy3 + " in the evening.")
+else:
+    print ("I guess I'd better have a " + favcandy4 + ", then.")
---
+++ assignments/Assignment 2.py
@@ -0,0 +1,7 @@
+def countdown():
+    n=1
+    while (n<11):
+        print (n)
+        n=n+1
+    print ("KABOOM!")
+countdown()
